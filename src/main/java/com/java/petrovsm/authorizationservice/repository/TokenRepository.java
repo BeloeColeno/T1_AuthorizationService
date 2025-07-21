135cc751be1d0afdc2d92d7b492c6283dd165185
BeloeColeno <semion.semen2013@gmail.com>
2025-07-21 23:35:32 +0500

    Refactor TokenRepository

--- src/main/java/com/java/petrovsm/authorizationservice/repository/TokenRepository.java
+++ src/main/java/com/java/petrovsm/authorizationservice/repository/TokenRepository.java
@@ -12,13 +12,10 @@ import java.util.Optional;
 
 @Repository
 public interface TokenRepository extends JpaRepository<Token, Long> {
-    List<Token> findByUserAndRevokedFalseAndTokenType(User user, Token.TokenType tokenType);
     Optional<Token> findByToken(String token);
-    boolean existsByTokenAndRevokedFalse(String token);
 
     @Query("SELECT t FROM Token t WHERE t.user.id = :userId AND t.revoked = false")
     List<Token> findAllValidTokensByUser(Long userId);
 
     void deleteByUserAndTokenType(User user, Token.TokenType tokenType);
-    void deleteByExpiryDateBefore(Instant expiryDate);
 }
