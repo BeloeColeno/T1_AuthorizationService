c6e9aa41df4d814cfc0e69b549115df4dcdd606a
BeloeColeno <semion.semen2013@gmail.com>
2025-07-21 23:25:39 +0500

    Add LoginRequest

---
+++ src/main/java/com/java/petrovsm/authorizationservice/dto/LoginRequest.java
@@ -0,0 +1,13 @@
+package com.java.petrovsm.authorizationservice.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    @NotBlank
+    private String login;
+
+    @NotBlank
+    private String password;
+}
