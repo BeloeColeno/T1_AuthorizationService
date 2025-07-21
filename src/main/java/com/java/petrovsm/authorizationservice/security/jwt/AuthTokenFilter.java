73fc2292bed194330c399f3246bd4e088bf3b3fc
BeloeColeno <semion.semen2013@gmail.com>
2025-07-21 23:27:12 +0500

    Add AuthTokenFilter

---
+++ src/main/java/com/java/petrovsm/authorizationservice/security/jwt/AuthTokenFilter.java
@@ -0,0 +1,69 @@
+package com.java.petrovsm.authorizationservice.security.jwt;
+
+import com.java.petrovsm.authorizationservice.service.TokenService;
+import com.java.petrovsm.authorizationservice.service.UserDetailsServiceImpl;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class AuthTokenFilter extends OncePerRequestFilter {
+
+    private final JwtUtils jwtUtils;
+    private final UserDetailsServiceImpl userDetailsService;
+    private final TokenService tokenService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        try {
+            String jwt = parseJwt(request);
+            if (jwt != null && jwtUtils.validateToken(jwt)) {
+                if (tokenService.isTokenRevoked(jwt)) {
+                    log.warn("Попытка использования отозванного токена");
+                } else {
+                    String username = jwtUtils.getUsernameFromToken(jwt);
+
+                    if (tokenService.isTokenExpiredByReLogin(jwt, username)) {
+                        log.warn("Попытка использования токена, созданного до последнего логина пользователя: {}", username);
+                    } else {
+                        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                                userDetails, null, userDetails.getAuthorities());
+                        authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                        SecurityContextHolder.getContext().setAuthentication(authentication);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            log.error("Не удалось установить аутентификацию пользователя: {}", e.getMessage());
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String parseJwt(HttpServletRequest request) {
+        String headerAuth = request.getHeader("Authorization");
+
+        if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
+            return headerAuth.substring(7);
+        }
+
+        return null;
+    }
+}
