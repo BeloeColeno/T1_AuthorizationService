e181d4cee61530925d39f3bd66be279c3aea98a0
BeloeColeno <semion.semen2013@gmail.com>
2025-07-21 23:26:28 +0500

    Add UserDetailsServiceImpl

---
+++ src/main/java/com/java/petrovsm/authorizationservice/service/UserDetailsServiceImpl.java
@@ -0,0 +1,34 @@
+package com.java.petrovsm.authorizationservice.service;
+
+import com.java.petrovsm.authorizationservice.model.User;
+import com.java.petrovsm.authorizationservice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        User user = userRepository.findByLogin(login)
+                .orElseThrow(() -> new UsernameNotFoundException("Пользователь не найден с логином: " + login));
+
+        List<SimpleGrantedAuthority> authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName().name()))
+                .toList();
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getLogin(),
+                user.getPassword(),
+                authorities);
+    }
+}
