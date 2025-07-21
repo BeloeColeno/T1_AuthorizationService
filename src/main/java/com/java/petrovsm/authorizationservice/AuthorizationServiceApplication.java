56309ef7761d871a6371d375489312a096251fe3
BeloeColeno <semion.semen2013@gmail.com>
2025-07-21 23:19:25 +0500

    Refactor AuthorizationServiceApplication

--- src/main/java/com/java/petrovsm/authorizationservice/AuthorizationServiceApplication.java
+++ src/main/java/com/java/petrovsm/authorizationservice/AuthorizationServiceApplication.java
@@ -2,10 +2,8 @@ package com.java.petrovsm.authorizationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class AuthorizationServiceApplication {
 
     public static void main(String[] args) {
